7ab6fe3797767c745e1ea5401295db064e417795
Vlad Vavshko <vava10041999@gmail.com>
2020-09-29 20:45:59 +0300

    creating header, and start working on main block

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  // Sticky header
+  window.addEventListener("scroll", () => {
+    let header = document.querySelector(".header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
+  //iTyped
+  ityped.init(document.querySelector(".ityped"), {
+    strings: ["лікарі", "пацієнти"],
+  });
+});
